93028f8e9af2d21f89e0adcabfb6577419b7b9fa
Vadym <vad.evlanov@gmail.com>
2021-01-24 09:36:32 +0200

    add eventlistener

--- src/components/ContactModal/ContactModal.js
+++ src/components/ContactModal/ContactModal.js
@@ -1,13 +1,33 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { editContacts } from '../../redux/contacts/contacts-operations';
 import s from './ContactModal.module.css';
+import { FaRegWindowClose } from 'react-icons/fa';
 
 export default function ContactModal({ contactData, onCloseModal }) {
   const { contact, id } = contactData;
   const [name, setName] = useState('');
   const [number, setNumber] = useState('');
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    const handlePressEsc = e => {
+      if (e.code === 'Escape') {
+        onCloseModal(false);
+      }
+    };
+
+    window.addEventListener('keydown', handlePressEsc);
+    return () => {
+      window.removeEventListener('keydown', handlePressEsc);
+    };
+  });
+
+  const handleBackDropClick = e => {
+    if (e.currentTarget === e.target) {
+      onCloseModal();
+    }
+  };
 
   const handleSave = () => {
     const updateContact = {
@@ -19,8 +39,15 @@ export default function ContactModal({ contactData, onCloseModal }) {
   };
 
   return (
-    <div className={s.overlay}>
+    <div className={s.overlay} onClick={e => handleBackDropClick(e)}>
       <div className={s.modal}>
+        <button
+          className={s.close}
+          type="button"
+          onClick={() => onCloseModal(false)}
+        >
+          <FaRegWindowClose size="30" className={s.closeIcon} />
+        </button>
         <form className={s.form}>
           <div className="inputWrapper">
             <label htmlFor="editName"></label>
